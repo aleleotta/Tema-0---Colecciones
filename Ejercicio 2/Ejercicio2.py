06ceeeec9d51c783f4a50536c5cb8fca647a48ba
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-26 15:59:22 +0200

    .

--- Ejercicio 2/Ejercicio2.py
+++ Ejercicio 2/Ejercicio2.py
@@ -2,3 +2,9 @@
 Crea un programa que pida diez números reales por teclado, los almacene en una lista,
 y luego lo recorra para averiguar el máximo y mínimo y mostrarlos por pantalla.
 """
+numbers = []
+for i in range(0, 10):
+    inputNum = int(input("Introduce number "+str(i)+": "))
+    numbers.append(inputNum)
+print(numbers)
+for currentNum in numbers:

d4c360204463cc078c74964f8eba6052904101d4
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-26 15:48:12 +0200

    .

--- Ejercicio 1/Ejercicio1.py
+++ Ejercicio 1/Ejercicio1.py
@@ -1,1 +1,9 @@
 """Crea una lista de enteros de longitud 10 que se inicializará con números aleatorios comprendidos entre 1 y 100."""
+import random
+
+randomNumbers = []
+randomNum = 0
+for i in range(0, 10):
+    randomNum = random.randint(0, 101)
+    randomNumbers.append(randomNum)
+print(randomNumbers)
---
+++ Ejercicio 5/Ejercicio5.py
@@ -0,0 +1,5 @@
+"""
+Crea un programa que cree una lista de enteros de tamaño 100 y lo rellene con valores enteros aleatorios entre 1 y 10
+(utiliza 1 + Math.random()*10).
+Luego pedirá un valor N y mostrará cuántas veces aparece N. 
+"""

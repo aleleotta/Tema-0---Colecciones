de5f6949c1f8a4687406d70c601443491552c0ea
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-27 15:27:40 +0200

    Update Ejercicio2.py

--- Ejercicio 2/Ejercicio2.py
+++ Ejercicio 2/Ejercicio2.py
@@ -7,4 +7,16 @@ for i in range(0, 10):
     inputNum = int(input("Introduce number "+str(i)+": "))
     numbers.append(inputNum)
 print(numbers)
+min = 0
+max = 0
+firstExec = 0
 for currentNum in numbers:
+    if firstExec == 0:
+        min = currentNum
+        firstExec = 1
+    if currentNum > max:
+        max = currentNum
+    if currentNum < min:
+        min = currentNum
+print("Max: "+str(max))
+print("Min: "+str(min))

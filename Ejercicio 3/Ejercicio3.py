27bf49feae963030da7d0565bcb2e8e365682673
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-28 15:59:20 +0200

    Update Ejercicio3.py

--- Ejercicio 3/Ejercicio3.py
+++ Ejercicio 3/Ejercicio3.py
@@ -2,3 +2,14 @@
 Realiza un programa que pida 8 números enteros y los almacene en una lista.
 A continuación, recorrerá esa tabla y mostrará esos números junto con la palabra “par” o “impar” según proceda.
 """
+numbers = []
+for i in range(1, 9):
+    num = int(input("Introduce a number "+str(i)+": "))
+    numbers.append(num)
+for currentNum in numbers:
+    if currentNum%2==1:
+        checkEven = "Odd"
+        print(currentNum, checkEven)
+    if currentNum%2==0:
+        checkEven = "Even"
+        print(currentNum, checkEven)

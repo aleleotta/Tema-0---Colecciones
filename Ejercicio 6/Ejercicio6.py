7a439591d9520c20cf6a56a18de310a8f830fbd6
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-02 15:25:40 +0200

    Update Ejercicio6.py

--- Ejercicio 6/Ejercicio6.py
+++ Ejercicio 6/Ejercicio6.py
@@ -2,3 +2,14 @@
 Escribe un programa que tome una cadena de texto como entrada y genere un diccionario que cuente la frecuencia
 de cada palabra en el texto.
 """
+text = input("Write a text: ")
+collection = text.split()
+word = input("Introduce the word you are looking for: ")
+found = 0
+for currentNum in collection:
+    if currentNum == word:
+        found = found + 1
+if found > 0:
+    print("The following word was found "+str(found)+" times.")
+else
+    print("The following word was not found within the text.")
